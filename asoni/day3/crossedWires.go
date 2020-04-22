74b95c3fbfc09938d28ee8c3ee4f1b9023c35dab
Abhay Soni <aymsoni29@gmail.com>
2020-04-21 18:30:41 -0700

    more commits

--- asoni/day3/crossedWires.go
+++ asoni/day3/crossedWires.go
@@ -94,9 +94,13 @@ func findMax(a int, b int) (c int) {
 }
 
 // O(n^2) search between each lines and finding intersections
-func findNearestIntersection(track1 [][]int, track2 [][]int) int {
+func findNearestIntersection(track1 [][]int, track2 [][]int) (int, int) {
 
 	nearestIntersection := math.MaxInt64
+	timingIntersection := math.MaxInt64
+
+	sum1 := 0
+	sum2 := 0
 
 	for i := 0; i < len(track1)-1; i++ {
 		point1 := track1[i]
@@ -112,6 +116,8 @@ func findNearestIntersection(track1 [][]int, track2 [][]int) int {
 		min1 := findMin(point1[1-commonIndex1], point2[1-commonIndex1])
 		max1 := findMax(point1[1-commonIndex1], point2[1-commonIndex1])
 		commonVal1 := point1[commonIndex1]
+		sum1 += max1-min1
+		sum2 = 0
 		// For each line of wire 1, check intersection with each line of wire 2
 		for j := 0; j < len(track2)-1; j++ {
 
@@ -126,9 +132,11 @@ func findNearestIntersection(track1 [][]int, track2 [][]int) int {
 			min2 := findMin(point3[1-commonIndex2], point4[1-commonIndex2])
 			max2 := findMax(point3[1-commonIndex2], point4[1-commonIndex2])
 			commonVal2 := point3[commonIndex2]
+			sum2 += max2-min2
 
 			var intersect [2]int
-			// These are parallel lines which will never intersect
+
+			// These are parallel lines which intersect only when they overlap
 			if commonIndex1 == commonIndex2 && point1[commonIndex1] == point3[commonIndex2] {
 				// Handle overlapping lines
 				globalMin := findMin(min1, min2)
@@ -137,6 +145,12 @@ func findNearestIntersection(track1 [][]int, track2 [][]int) int {
 					if min1 <= k && k <= max1 && min2 <= k && k <= max2 {
 						intersect[commonIndex2] = point3[commonIndex2]
 						intersect[1-commonIndex2] = k
+						sum1 = revertNumber(sum1)
+						sum2 = revertNumber(sum2)
+						fmt.Println("Sum1 ->",sum1, "Sum2 ->",sum2)
+						if sum1+sum2 <= timingIntersection {
+							timingIntersection = sum1+sum2
+						}
 						break
 					}
 				}
@@ -148,27 +162,40 @@ func findNearestIntersection(track1 [][]int, track2 [][]int) int {
 				if (min1 <= probableIntersection[commonIndex2] && probableIntersection[commonIndex2] <= max1) && (min2 <= probableIntersection[commonIndex1] && probableIntersection[commonIndex1] <= max2) {
 					intersect[0] = probableIntersection[0]
 					intersect[1] = probableIntersection[1]
+					sum1 = revertNumber(sum1)
+					sum2 = revertNumber(sum2)
+					fmt.Println("Sum1 ->",sum1, "Sum2 ->",sum2)
+					if sum1+sum2 <= timingIntersection {
+						timingIntersection = sum1+sum2
+					}
 				} else {
 					continue
 				}
 			}
-			if intersect[0] < 0 {
-				intersect[0] = -intersect[0]
-			}
-			if intersect[1] < 0 {
-				intersect[1] = -intersect[1]
-			}
+
+			
+			intersect[0] = revertNumber(intersect[0])
+			intersect[1] = revertNumber(intersect[1])
+			sum1 = revertNumber(sum1)
+			sum2 = revertNumber(sum2)
 
 			if intersect[0]+intersect[1] != 0 {
 				nearestIntersection = findMin(intersect[0]+intersect[1], nearestIntersection)
 			}
 			// fmt.Println()
+			
 		}
 	}
 
-	return nearestIntersection
+	return nearestIntersection, timingIntersection
 }
 
+func revertNumber(x int) (int) {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
 func main() {
 	wires := parseInput("crossedWires_input.txt")
 	track1 := trackWire(wires[0])
