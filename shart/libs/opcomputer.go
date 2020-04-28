702f2fd3075897bdf212a5e469bad0eea185cd1f
Sean Hart <sean.hart@getcruise.com>
2020-04-27 17:52:50 -0700

    WIP - Tests Fail, but function

--- shart/libs/opcomputer.go
+++ shart/libs/opcomputer.go
@@ -6,9 +6,9 @@ type Parameters struct {
 }
 
 // RunComputer will run opcode till termination
-func RunComputer(memory []int, instructionPointer int) (output []int, nextPointer int) {
+func RunComputer(inputInt int, memory []int, instructionPointer int) (outputInt int, outputMem []int, nextPointer int) {
 	if memory[instructionPointer] == 99 {
-		return memory, 99
+		return outputInt, memory, 99
 	}
 	opcode := memory[instructionPointer]
 	rawParams := memory[instructionPointer+1 : instructionPointer+4]
@@ -16,18 +16,19 @@ func RunComputer(memory []int, instructionPointer int) (output []int, nextPointe
 
 	switch opcode {
 	case 99:
-		return memory, 99
+		return outputInt, memory, 99
 	case 1:
 		memory[inst.storeReg] = memory[inst.leftReg] + memory[inst.rightReg]
 	case 2:
 		memory[inst.storeReg] = memory[inst.leftReg] * memory[inst.rightReg]
 	}
 	if memory[instructionPointer+4] != 99 {
-		RunComputer(memory, instructionPointer+4)
+		RunComputer(inputInt, memory, instructionPointer+4)
 	}
-	return memory, 99
+	return outputInt, memory, 99
 }
 
+//ResetMem will reset memory to a known state.
 func ResetMem(memory []int) (newMem []int) {
 	newMem = make([]int, len(memory))
 	copy(newMem, memory)
