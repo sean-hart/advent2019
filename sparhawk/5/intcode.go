e4f0d26ce89561b728a55c8c39c819feb0e05fe3
Mark Sparhawk <mark.sparhawk@getcruise.com>
2020-04-30 13:28:08 -0700

    Day 2 solution

--- sparhawk/5/intcode.go
+++ sparhawk/5/intcode.go
@@ -1,102 +1,136 @@
 package main
 
 type program struct {
-	code []int
-	cur int // Execution cursor location
-	input int
+	code   []int
+	cur    int
+	input  int
 	output int
-	mode int
 }
 
-func add(p program) program {
-	param1, param2, loc := getParams(p)
-	p.code[loc] = param1 + param2
+//Instruction 1
+func (p *program) add() {
+	p.code[p.paramLocation(3)] = p.code[p.paramLocation(1)] + p.code[p.paramLocation(2)]
 	p.cur += 4
-	return p
 }
 
-func multiply(p program) program {
-	param1, param2, loc := getParams(p)
-	p.code[loc] = param1 * param2
+//Instruction 2
+func (p *program) multiply() {
+	p.code[p.paramLocation(3)] = p.code[p.paramLocation(1)] * p.code[p.paramLocation(2)]
 	p.cur += 4
-	return p
 }
 
-func getParams(p program) (int, int, int) {
-
-	param1 := p.code[p.cur+1]
-	if (p.code[p.cur] / 100 % 10) == 0 {
-		param1 = p.code[param1]
-	}
-
-	param2 := p.code[p.cur+2]
-	if (p.code[p.cur] / 1000 % 10) == 0 {
-		param2 = p.code[param2]
-	}
-
-	loc := p.code[p.cur+3]
-	if (p.code[p.cur] / 10000 % 10) == 1 {
-		loc = p.code[loc]
-	}
-	
-	return param1, param2, loc
-}
-
-func write(p program) program {
-	p.code[p.code[p.cur+1]] = p.input
+//Instruction 3
+func (p *program) write() {
+	p.code[p.paramLocation(1)] = p.input
 	p.cur += 2
-	return p
 }
 
-func read(p program) program {
-	p.output = p.code[p.code[p.cur+1]]
+//Instruction 4
+func (p *program) read() {
+	p.output = p.code[p.paramLocation(1)]
 	p.cur += 2
-	return p
 }
 
-func intcode(p program) program {
+//Instruction 5
+func (p *program) jumpIfTrue() {
+	if p.code[p.paramLocation(1)] != 0 {
+		p.cur = p.code[p.paramLocation(2)]
+	} else {
+		p.cur += 3
+	}
+}
+
+//Instruction 6
+func (p *program) jumpIfFalse() {
+	if p.code[p.paramLocation(1)] == 0 {
+		p.cur = p.code[p.paramLocation(2)]
+	} else {
+		p.cur += 3
+	}
+}
+
+//Instruction 7
+func (p *program) lessThan() {
+	if p.code[p.paramLocation(1)] < p.code[p.paramLocation(2)] {
+		p.code[p.paramLocation(3)] = 1
+	} else {
+		p.code[p.paramLocation(3)] = 0
+	}
+	p.cur += 4
+}
+
+//Instruction 8
+func (p *program) equal() {
+	if p.code[p.paramLocation(1)] == p.code[p.paramLocation(2)] {
+		p.code[p.paramLocation(3)] = 1
+	} else {
+		p.code[p.paramLocation(3)] = 0
+	}
+	p.cur += 4
+}
+
+type inst struct {
+	opcode int
+	params []int
+}
+
+func (p *program) intcode() {
 	op := p.code[p.cur] % 100
 	switch op {
 	case 1:
-		return add(p)
+		p.add()
 	case 2:
-		return multiply(p)
+		p.multiply()
 	case 3:
-		return write(p)
+		p.write()
 	case 4:
-		return read(p)
+		p.read()
+	case 5:
+		p.jumpIfTrue()
+	case 6:
+		p.jumpIfFalse()
+	case 7:
+		p.lessThan()
+	case 8:
+		p.equal()
 	case 99:
-		return p
-	default:
-		return p
+		return
 	}
 }
 
-func runProgram(p program) program {
+func (p *program) runProgram() {
 	for p.cur < len(p.code) {
 		if p.code[p.cur] != 99 {
-			p = intcode(p)
+			p.intcode()
 		} else {
-			return p
+			return
 		}
 	}
+	return
+}
+
+func (p *program) paramLocation(offset int) int {
+
+	// Set location to memory address stored in cur + offset assuming positional mode
+	location := p.code[p.cur+offset]
+
+	// If we're in immediate mode, the parameter is simply stored at the cur + offset
+	if (p.code[p.cur] / Pow(10, offset+1) % 10) == 1 {
+		location = p.cur + offset
+	}
+
+	return location
+}
+
+func Pow(a, b int) int {
+	//Pow() Integer power function a^b.
+	p := 1
+	for b > 0 {
+		if b&1 != 0 {
+			p *= a
+		}
+		b >>= 1
+		a *= a
+	}
 	return p
-}
-
-func getDigit(num, pos int) int {
-	s := intToSlice(num)
-	if pos > len(s) {
-		return 0
-	}
-	return s[pos]
-}
-
-func intToSlice(num int) []int {
-	res := []int{}
-	for num / 10 > 0 {
-		res = append([]int{num % 10}, res...)
-		num /= 10
-	}
-	res = append([]int{num % 10}, res...)
-	return res
 }
