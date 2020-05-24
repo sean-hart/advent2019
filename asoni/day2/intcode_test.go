e5053f9c8f4641e8dbc07f23ce385a4021d6816f
Abhay Soni <aymsoni29@gmail.com>
2020-05-23 17:43:38 -0700

    Add code Init

---
+++ asoni/day2/intcode_test.go
@@ -0,0 +1,37 @@
+package day2
+
+import (
+	"testing"
+)
+
+func TestReadCsvFile(t *testing.T) {
+
+}
+
+func BenchmarkReadCsvFile(b *testing.B) {
+
+}
+
+func TestParseData(t *testing.T) {
+
+}
+
+func BenchmarkParseData(b *testing.B) {
+
+}
+
+func TestRunOpCode(t *testing.T) {
+
+}
+
+func BenchmarkRunOpCode(b *testing.B) {
+
+}
+
+func TestPart2(t *testing.T) {
+
+}
+
+func BenchmarkPart2(b *testing.B) {
+
+}
